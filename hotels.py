ee8f71c5104f22a998afe590dd7bc562c3fb20ee
alexvm753 <LexDarkMan@gmail.com>
2026-03-24 18:46:06 +0300

    backendCourse FastAPI lesson 7 pagination

--- hotels.py
+++ hotels.py
@@ -1,13 +1,19 @@
 from fastapi import Query, Body, APIRouter
 
+from schemas.hotels import Hotel, HotelPATCH
+
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
 hotels = [
-    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 1, "title": "Сочи", "name": "sochi"},
     {"id": 2, "title": "Дубай", "name": "dubay"},
+    {"id": 3, "title": "Мальдивы", "name": "maldiv"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
+    {"id": 8, "title": "Кипр", "name": "cyprus"},
 ]
-
-
 
 @router.get(
     "",
@@ -17,7 +23,9 @@ hotels = [
 def get_hotels(
         id: int | None = Query(None, description="Идентификатор отеля"),
         title: str | None = Query(None, description = "Название отеля"),
-        name: str | None = Query(None, description = "Наименование отеля")
+        name: str | None = Query(None, description = "Наименование отеля"),
+        page: int | None = Query(None, gt =0, description = "Номер страницы"),
+        per_page: int | None = Query(None, gt = 0, lt = 101, description = "Количество на странице"),
 ):
     hotels_=[]
     for hotel in hotels:
@@ -28,18 +36,28 @@ def get_hotels(
         if name and hotel["name"] != name:
             continue
         hotels_.append(hotel)
+    if page and per_page:
+        return hotels_[(page - 1) * per_page : per_page * page]
+        #return hotels_[(page - 1) * per_page:][:per_page]
     return hotels_
 
 @router.post("", summary="Добавление нового отеля")
-def create_hotel(
-        title: str = Body(embed = True),
-        name: str = Body(embed = True),
+def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+    "1": {"summary": "Сочи", "value":{
+        "title": "Отель Сочи 5 звезд у моря",
+        "name": "sochi_u_morya",
+    }},
+    "2": {"summary": "Дубай", "value":{
+        "title": "Дубай 5 звезд оазис",
+        "name": "dubai_oazis",
+    }},
+})
 ):
     global hotels
     hotels.append({
         "id": hotels[-1]["id"] + 1,
-        "title": title,
-        "name": name
+        "title": hotel_data.title,
+        "name": hotel_data.name
     })
     return {"status": "ok"}
 
@@ -50,31 +68,26 @@ def delete_hotel(hotel_id: int):
     return {"status": "ok"}
 
 @router.put("/{hotel_id}", summary="Изменение отеля")
-def update_hotel(
-        hotel_id: int,
-        title: str = Body(embed = True),
-        name: str = Body(embed = True),
-):
+def update_hotel(hotel_id: int, hotel_data: Hotel):
     global hotels
     #hotels = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
     for hotel in hotels:
         if hotel["id"] == hotel_id:
-            hotel["title"] = title
-            hotel["name"] = name
+            hotel["title"] = hotel_data.title
+            hotel["name"] = hotel_data.name
     return {"status": "ok"}
 
 @router.patch("/hotels/{hotel_id}", summary="Частичное изменение данных об отеле")
 def update_hotel(
         hotel_id: int,
-        title: str | None = Body(None, embed = True),
-        name: str | None = Body(None, embed = True),
+        hotel_data: HotelPATCH
 ):
     global hotels
     # hotels = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
     for hotel in hotels:
         if hotel["id"] == hotel_id:
-            if title:
-                hotel["title"] = title
-            if name:
-                hotel["name"] = name
+            if hotel_data.title:
+                hotel["title"] = hotel_data.title
+            if hotel_data.name:
+                hotel["name"] = hotel_data.name
     return {"status": "ok"}
