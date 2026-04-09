e0e901bd16272848ce16af207d1178e19ae538f3
alexvm753 <LexDarkMan@gmail.com>
2026-04-09 18:02:29 +0300

    backendCourse DB lesson 10 pattern repository (select, insert, update, delete)

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,9 +1,6 @@
 from fastapi import Query, Body, APIRouter
 
-from sqlalchemy import insert, select, func
-
-from database import async_session_maker, engine
-from src.models.hotels import HotelsOrm
+from database import async_session_maker
 from repositories.hotels import HotelsRepository
 from src.api.dependencies import PaginationDep
 from src.schemas.hotels import Hotel, HotelPATCH
@@ -41,7 +38,10 @@ async def get_hotels(
             offset=(pagination.page - 1) * per_page
         )
 
-
+@router.get("/{hotel_id}")
+async def get_hotel(hotel_id: int):
+    async with async_session_maker() as session:
+        return await HotelsRepository(session).get_one_or_none(id=hotel_id)
 @router.post("", summary="Добавление нового отеля")
 async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     "1": {"summary": "Сочи", "value":{
@@ -60,32 +60,39 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     return {"status": "ok", "data": hotel}
 
 @router.delete("/{hotel_id}", summary="Удаление отеля")
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+async def delete_hotel(hotel_id: int):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).delete(id=hotel_id)
+        await session.commit()
     return {"status": "ok"}
 
 @router.put("/{hotel_id}", summary="Изменение отеля")
-def update_hotel(hotel_id: int, hotel_data: Hotel):
-    global hotels
-    #hotels = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            hotel["title"] = hotel_data.title
-            hotel["name"] = hotel_data.name
+async def update_hotel(hotel_id: int, hotel_data: Hotel):
+    # global hotels
+    # #hotels = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    # for hotel in hotels:
+    #     if hotel["id"] == hotel_id:
+    #         hotel["title"] = hotel_data.title
+    #         hotel["name"] = hotel_data.name
+    async with async_session_maker() as session:
+        await HotelsRepository(session).update(hotel_data, id=hotel_id)
+        await session.commit()
     return {"status": "ok"}
 
 @router.patch("/hotels/{hotel_id}", summary="Частичное изменение данных об отеле")
-def update_hotel(
+async def update_hotel(
         hotel_id: int,
         hotel_data: HotelPATCH
 ):
-    global hotels
-    # hotels = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            if hotel_data.title:
-                hotel["title"] = hotel_data.title
-            if hotel_data.name:
-                hotel["name"] = hotel_data.name
+    # global hotels
+    # # hotels = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    # for hotel in hotels:
+    #     if hotel["id"] == hotel_id:
+    #         if hotel_data.title:
+    #             hotel["title"] = hotel_data.title
+    #         if hotel_data.name:
+    #             hotel["name"] = hotel_data.name
+    async with async_session_maker() as session:
+        await HotelsRepository(session).update(hotel_data, exclude_unset=True, id=hotel_id)
+        await session.commit()
     return {"status": "ok"}
---
+++ src/repositories/base.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from sqlalchemy import select, insert, delete, update
+
+class BaseRepository:
+    model = None
+
+    def __init__(self, session):
+        self.session = session
+
+    async def get_all(self, *args, **kwargs):
+        query = select(self.model)
+        result = await self.session.execute(query)
+        return result.scalars().all()
+
+    async def get_one_or_none(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        return result.scalars().one_or_none()
+
+    async def add(self, data: BaseModel):
+        add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        # print(add_data_stmt.compile(engine, compile_kwargs={"literal_binds": True})) # скомпилировать и распечатать запрос в БД
+        result = await self.session.execute(add_data_stmt)
+        return result.scalars().one()
+
+    async def update(self, data: BaseModel, exclude_unset: bool=False, **filter_by) -> None:
+        query = update(self.model).filter_by(**filter_by).values(**data.model_dump(exclude_unset=exclude_unset)) # exclude_unset - исключает из запроса пропущенные (непереданные) параметры
+        await self.session.execute(query)
+
+    async def delete(self, **filter_by) -> None:
+        query = delete(self.model).filter_by(**filter_by)
+        await self.session.execute(query)
