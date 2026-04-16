65d072356ac1173ca30e67bbccd0950fa5f77a0b
alexvm753 <LexDarkMan@gmail.com>
2026-04-16 12:23:19 +0300

    backendCourse authorization & authentification lesson 2 register user

--- src/main.py
+++ src/main.py
@@ -9,10 +9,12 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.api.auth import router as router_auth
 from src.api.hotels import router as router_hotels
 
 app = FastAPI(docs_url=None) #app = FastAPI()
 
+app.include_router(router_auth)
 app.include_router(router_hotels)
 
 @app.get("/docs", include_in_schema=False)
---
+++ src/repositories/users.py
@@ -0,0 +1,9 @@
+from src.schemas.users import User
+from src.models.users import UsersOrm
+from src.repositories.base import BaseRepository
+
+
+
+class UsersRepository(BaseRepository):
+    model = UsersOrm
+    schema = User
